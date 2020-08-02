6d5163587f868691aa2815bb37fab864e14dd32d
rafaelrc7 <rafaelrc7@gmail.com>
2020-08-01 21:05:18 -0300

    Fixed identation and added bindings

--- config.h
+++ config.h
@@ -1,16 +1,16 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static unsigned int borderpx  		= 3;        /* border pixel of windows */
-static unsigned int snap      		= 32;       /* snap pixel */
+static unsigned int borderpx        = 3;        /* border pixel of windows */
+static unsigned int snap            = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int gappih    = 4;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 4;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static int showbar            		= 1;        /* 0 means no bar */
-static int topbar             		= 1;        /* 0 means bottom bar */
+static int showbar                  = 1;        /* 0 means no bar */
+static int topbar                   = 1;        /* 0 means bottom bar */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
@@ -23,7 +23,7 @@ static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#005577";
 static char selbgcolor[]            = "#005577";
-static char urgbordercolor[]   		= "#ff0000";
+static char urgbordercolor[]        = "#ff0000";
 static char urgfgcolor[]            = "#bbbbbb";
 static char urgbgcolor[]            = "#222222";
 static const unsigned int baralpha = 0xd0;
@@ -53,14 +53,14 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1:", "2:", "3:", "4:", "5:", "6:", "7:", "8:", "9:" };
+static const char *tags[] = { "1:", "2:", "3:", "4:", "5:", "6:", "7:", "8:", "9:" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	/* class              instance    title      tags mask   isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",             NULL,       NULL,       0,            1,           0,         0,        -1 },
 	{ "firefox",          NULL,       NULL,       1 << 1,       0,           0,         0,        -1 },
 	{ "St",               NULL,       NULL,       0,            0,           1,         1,        -1 },
@@ -70,8 +70,10 @@ static const Rule rules[] = {
 	{ "Galculator",       NULL,       NULL,       0,            1,           0,         0,        -1 },
 	{ "KeePassXC",        NULL,       NULL,       1 << 8,       0,           0,         0,        -1 },
 	{ "Thunderbird",      NULL,       NULL,       1 << 8,       0,           0,         0,        -1 },
-	{ "Pavucontrol",      NULL,       NULL,       0,			1,           0,         0,        -1 },
-	{ "htop",			  NULL,       NULL,       0,			1,           0,         0,        -1 },
+	{ "Pavucontrol",      NULL,       NULL,       0,            1,           0,         0,        -1 },
+	{ "htop",             NULL,       NULL,       0,            1,           0,         0,        -1 },
+	{ "mpd",              NULL,       NULL,       0,            1,           0,         0,        -1 },
+	{ "Spotify",          NULL,       NULL,       1 << 4,       0,           0,         0,        -1 },
 	{ NULL,               NULL,   "Event Tester", 0,            0,           0,         1,        -1 }, /* xev */
 };
 
@@ -142,13 +144,13 @@ ResourcePref resources[] = {
 		{ "urgbgcolor",         STRING,  &urgbgcolor},
 		{ "urgbordercolor",     STRING,  &urgbordercolor},
 		{ "urgfgcolor",         STRING,  &urgfgcolor},
-		{ "borderpx",          	INTEGER, &borderpx },
-		{ "snap",          		INTEGER, &snap },
-		{ "showbar",          	INTEGER, &showbar },
-		{ "topbar",          	INTEGER, &topbar },
-		{ "nmaster",          	INTEGER, &nmaster },
-		{ "resizehints",       	INTEGER, &resizehints },
-		{ "mfact",      	 	FLOAT,   &mfact },
+		{ "borderpx",           INTEGER, &borderpx },
+		{ "snap",               INTEGER, &snap },
+		{ "showbar",            INTEGER, &showbar },
+		{ "topbar",             INTEGER, &topbar },
+		{ "nmaster",            INTEGER, &nmaster },
+		{ "resizehints",        INTEGER, &resizehints },
+		{ "mfact",              FLOAT,   &mfact },
 		{ "alpha",              ALPHA,   alphas},
 };
 
@@ -175,20 +177,20 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ALTKEY,              	XK_h,      incrgaps,       {.i = +1 } },
-	{ MODKEY|ALTKEY,              	XK_l,      incrgaps,       {.i = -1 } },
-	{ MODKEY|ALTKEY|ShiftMask,    	XK_h,      incrogaps,      {.i = +1 } },
-	{ MODKEY|ALTKEY|ShiftMask,    	XK_l,      incrogaps,      {.i = -1 } },
-	{ MODKEY|ALTKEY|ControlMask,  	XK_h,      incrigaps,      {.i = +1 } },
-	{ MODKEY|ALTKEY|ControlMask,  	XK_l,      incrigaps,      {.i = -1 } },
-	{ MODKEY|ALTKEY,              	XK_0,      togglegaps,     {0} },
-	{ MODKEY|ALTKEY|ShiftMask,    	XK_0,      defaultgaps,    {0} },
+	{ MODKEY|ALTKEY,                XK_h,      incrgaps,       {.i = +1 } },
+	{ MODKEY|ALTKEY,                XK_l,      incrgaps,       {.i = -1 } },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_h,      incrogaps,      {.i = +1 } },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_l,      incrogaps,      {.i = -1 } },
+	{ MODKEY|ALTKEY|ControlMask,    XK_h,      incrigaps,      {.i = +1 } },
+	{ MODKEY|ALTKEY|ControlMask,    XK_l,      incrigaps,      {.i = -1 } },
+	{ MODKEY|ALTKEY,                XK_0,      togglegaps,     {0} },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
 	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
 	{ MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
-	{ MODKEY|ALTKEY,              	XK_y,      incrohgaps,     {.i = +1 } },
-	{ MODKEY|ALTKEY,              	XK_o,      incrohgaps,     {.i = -1 } },
+	{ MODKEY|ALTKEY,                XK_y,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|ALTKEY,                XK_o,      incrohgaps,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
@@ -207,18 +209,22 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,						XK_Right,  shiftview,	   {.i = +1}  },
-	{ MODKEY,						XK_Left,   shiftview,	   {.i = -1}  },
+	{ MODKEY,                       XK_Right,  shiftview,      {.i = +1}  },
+	{ MODKEY,                       XK_Left,   shiftview,      {.i = -1}  },
 	{ MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
-	{ 0, 							XF86XK_AudioMute,			spawn,		SHCMD("amixer -q set Master toggle; kill -37 $(pidof dwmblocks)") },
-	{ 0, 							XF86XK_AudioRaiseVolume,	spawn,		SHCMD("amixer -q set Master 5%+; kill -37 $(pidof dwmblocks)") },
-	{ 0, 							XF86XK_AudioLowerVolume,	spawn,		SHCMD("amixer -q set Master 5%-; kill -37 $(pidof dwmblocks)") },
-	{ 0, 							XF86XK_AudioPrev,			spawn,		SHCMD("mpc prev;sp prev") },
-	{ 0, 							XF86XK_AudioNext,			spawn,		SHCMD("mpc next;sp next") },
-	{ 0, 							XF86XK_AudioPause,			spawn,		SHCMD("mpc pause;sp pause") },
-	{ 0, 							XF86XK_AudioPlay,			spawn,		SHCMD("mpc play;sp play") },
-	{ MODKEY,						XF86XK_AudioPlay,			spawn,		SHCMD("spotify") },
-	{ 0, 							XF86XK_AudioStop,			spawn,		SHCMD("mpc stop;pkill -x spotify") },
+	{ 0,                            XF86XK_AudioMute,           spawn,    SHCMD("amixer -q set Master toggle; kill -37 $(pidof dwmblocks)") },
+	{ 0,                            XF86XK_AudioRaiseVolume,    spawn,    SHCMD("amixer -q set Master 5%+; kill -37 $(pidof dwmblocks)") },
+	{ 0,                            XF86XK_AudioLowerVolume,    spawn,    SHCMD("amixer -q set Master 5%-; kill -37 $(pidof dwmblocks)") },
+	{ ShiftMask,                    XF86XK_AudioRaiseVolume,    spawn,    SHCMD("amixer -q set Master 100%; kill -37 $(pidof dwmblocks)") },
+	{ MODKEY,                       XF86XK_AudioRaiseVolume,    spawn,    SHCMD("pavucontrol") },
+	{ 0,                            XF86XK_AudioPrev,           spawn,    SHCMD("mpc prev;sp prev") },
+	{ 0,                            XF86XK_AudioNext,           spawn,    SHCMD("mpc next;sp next") },
+	{ 0,                            XF86XK_AudioPause,          spawn,    SHCMD("mpc pause;sp pause") },
+	{ 0,                            XF86XK_AudioPlay,           spawn,    SHCMD("mpc play;sp play") },
+	{ MODKEY|ShiftMask,             XF86XK_AudioPlay,           spawn,    SHCMD("spotify") },
+	{ MODKEY,                       XF86XK_AudioPlay,           spawn,    SHCMD("st -c mpd -e ncmpcpp") },
+	{ 0,                            XF86XK_AudioStop,           spawn,    SHCMD("mpc stop;pkill -x spotify") },
+	{ ControlMask|ShiftMask,        XK_Escape,                  spawn,    SHCMD("st -c htop -e htop") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -243,6 +249,8 @@ static Button buttons[] = {
 	{ ClkStatusText,        0,              Button3,        sigdwmblocks,   {.i = 3} },
 	{ ClkStatusText,        0,              Button4,        sigdwmblocks,   {.i = 4} },
 	{ ClkStatusText,        0,              Button5,        sigdwmblocks,   {.i = 5} },
+	{ ClkStatusText,        ShiftMask,      Button1,        sigdwmblocks,   {.i = 6} },
+	{ ClkStatusText,        ShiftMask,      Button3,        sigdwmblocks,   {.i = 7} },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
